0ba6647e5d466845193284656eb7101629d919d9
MrBercisk <bimosatrio814@gmail.com>
2021-10-05 19:11:51 +0700

    Menambahkan semua isi folder

---
+++ application/views/v_about.php
@@ -0,0 +1,26 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type='disc'>
+        <li>Nama Depan : Bimo</li>
+        <li>Nama Belakang : Satrio</li>
+    </ul>
+    <br>
+
+    <h4>Alamat</h4>
+    <ul type='none'>
+        <li>Jalan Daendels Purworejo Jawa Tengah</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type='none'>
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type='square'>
+        <li>Beladiri</li>
+        <li>Catur</li>
+        <li>Sepak Bola</li>
+    </ul>
+</section>
