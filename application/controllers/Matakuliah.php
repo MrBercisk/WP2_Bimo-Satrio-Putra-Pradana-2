2fbdf51e0e1099585bb01e6d2f9dac753bf62765
MrBercisk <bimosatrio814@gmail.com>
2021-10-15 10:37:36 +0700

    Menambahkan Validasi Controller & view

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -8,18 +8,46 @@ class Matakuliah extends CI_Controller
 
     public function cetak()
     {
-        $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required|min_lenght[3]', [
-            'required' => 'Kode Matakuliah Harus Diisi',
-            'min_lenght' => 'Kode terlalu pendek'
-        ]);
-        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required|min_lenght[3]', [
-            'required' => 'Nama Matakuliah Harus Diisi',
-            'min_lenght' => 'Nama terlalu pendek'
-        ]);
 
-        if ($this->form_validation->run() != true) {
+
+
+        $this->form_validation->set_rules(
+            'kode',
+            'Kode',
+            'trim|required|min_lenght[3]',
+            array(
+                'required' => '%s Wajib diisi.',
+                'min_lenght' => '%s Terlalu pendek.'
+            )
+
+        );
+        $this->form_validation->set_rules(
+            'nama',
+            'Nama Matakuliah',
+            'required',
+            array(
+                'required' => '%s Wajib diisi.',
+                'min_lenght' => '%s terlalu pendek'
+            )
+
+        );
+        $this->form_validation->set_rules(
+            'sks',
+            'SKS',
+            'required',
+            array(
+                'required' => '%s Wajib diisi.',
+            )
+
+        );
+
+
+        if ($this->form_validation->run() != TRUE) {
             $this->load->view('view-form-matakuliah');
         } else {
+            echo $this->input->post('nama');
+            exit();
+
             $data = [
                 'kode' => $this->input->post('kode'),
                 'nama' => $this->input->post('nama'),
