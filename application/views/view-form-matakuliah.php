03f6f6dea62590b83d7447e24a18d9cae72a3413
MrBercisk <bimosatrio814@gmail.com>
2021-10-15 10:42:36 +0700

    Menambahkan controller dan view

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -23,15 +23,17 @@
                     <th>Kode MTK</th>
                     <th>:</th>
                     <td>
-                        <input type="text" name="kode" id="kode">
+                        <input type="text" name="kode" id="kode" value="<?= set_value('kode'); ?>" placeholder="Required">
                     </td>
+                    <td><?php echo form_error('kode'); ?></td>
                 </tr>
                 <tr>
-                    <th>Kode MTK</th>
+                    <th>Nama MTK</th>
                     <th>:</th>
                     <td>
-                        <input type="text" name="nama" id="nama">
+                        <input type="text" name="nama" id="nama" value="<?= set_value('kode'); ?>" placeholder="Required">
                     </td>
+                    <td><?php echo form_error('nama'); ?></td>
                 </tr>
                 <tr>
                     <th>SKS</th>
@@ -44,6 +46,7 @@
                             <option value="4">4</option>
                         </select>
                     </td>
+                    <td><?php echo form_error('sks'); ?></td>
                 </tr>
                 <tr>
                     <td colspan="3" align="center">
