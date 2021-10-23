0d78465f08be2944b9214fe2bccccfec02fcf72b
MrBercisk <bimosatrio814@gmail.com>
2021-10-24 01:13:49 +0700

    Menambahkan ModelUser dan ModelBuku

--- application/models/ModelBuku.php
+++ application/models/ModelBuku.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class ModelBuku extends CI_Model
@@ -9,26 +9,41 @@ class ModelBuku extends CI_Model
     {
         return $this->db->get('buku');
     }
+
     public function bukuWhere($where)
     {
-        return $this->db->get_where('buku' , $where);
+        return $this->db->get_where('buku', $where);
     }
+
     public function simpanBuku($data = null)
     {
-        $this->db->insert('buku',$data);
+        $this->db->insert('buku', $data);
     }
+
+    public function updateBuku($data = null, $where = null)
+    {
+        $this->db->update('buku', $data, $where);
+    }
+
     public function hapusBuku($where = null)
     {
-        $this->db->delete('buku',$where);
+        $this->db->delete('buku', $where);
     }
+
     public function total($field, $where)
     {
         $this->db->select_sum($field);
-        if(!empty($where) && count($where) > 0){
+        if (!empty($where) && count($where) > 0) {
             $this->db->where($where);
         }
         $this->db->from('buku');
         return $this->db->get()->row($field);
+    }
+
+    public function getLimitBuku()
+    {
+        $this->db->limit(5);
+        return $this->db->get('buku');
     }
 
     //manajemen kategori
@@ -36,18 +51,22 @@ class ModelBuku extends CI_Model
     {
         return $this->db->get('kategori');
     }
-    public function kategoriuWhere($where)
+
+    public function kategoriWhere($where)
     {
-        return $this->db->get_where('kategori' , $where);
+        return $this->db->get_where('kategori', $where);
     }
+
     public function simpanKategori($data = null)
     {
-        $this->db->insert('kategori',$data);
+        $this->db->insert('kategori', $data);
     }
+
     public function hapusKategori($where = null)
     {
-        $this->db->delete('kategori',$where);
+        $this->db->delete('kategori', $where);
     }
+
     public function updateKategori($where = null, $data = null)
     {
         $this->db->update('kategori', $data, $where);
@@ -56,12 +75,10 @@ class ModelBuku extends CI_Model
     //join
     public function joinKategoriBuku($where)
     {
-        $this->db->select('buku.id_kategori,kategori.kategori');
+        $this->db->select('*');
         $this->db->from('buku');
-        $this->db->join('kategori', 'kategori.id buku.id_kategori');
+        $this->db->join('kategori', 'kategori.id = buku.id_kategori');
         $this->db->where($where);
         return $this->db->get();
     }
-   
-
 }
--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class ModelUser extends CI_Model
@@ -7,20 +7,31 @@ class ModelUser extends CI_Model
     {
         $this->db->insert('user', $data);
     }
+
+    //menambah method cek data
     public function cekData($where = null)
     {
-        return $this->db->get_where('user' , $where);
+        return $this->db->get_where('user', $where);
     }
+
     public function getUserWhere($where = null)
     {
-        return $this->db->get_where('user' , $where);
+        return $this->db->get_where('user', $where);
     }
+
     public function cekUserAccess($where = null)
     {
         $this->db->select('*');
-        $this->db->from('user');
-        $this->db->limit(10,0);
+        $this->db->from('acccess_menu');
+        $this->db->where($where);
         return $this->db->get();
     }
 
+    public function getUserLimit()
+    {
+        $this->db->select('*');
+        $this->db->from('user');
+        $this->db->limit(10, 0);
+        $this->db->get();
+    }
 }
